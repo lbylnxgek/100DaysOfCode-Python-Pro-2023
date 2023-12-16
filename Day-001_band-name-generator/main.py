2f61965a2de61d579386e731986ea172445a4af6
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2023-12-16 14:55:04 -0700

    Add day-001 code and update README.md

---
+++ Day-001_band-name-generator/main.py
@@ -0,0 +1,11 @@
+# Create a greeting.
+print("Welcome to the Cover Band generator.")
+
+# Ask user for the city they grew up in.
+user_city = input("What city did you grow up in?\n")
+
+# Ask the user for the name of a pet.
+user_pet = input("What is the name of a pet?\n")
+
+# Combine the name of their city and pet to produce a band name.
+print("\nYour new cover band name is: " + user_city + " " + user_pet)
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 100 Days of Code - The Complete Python Pro Bootcamp (2023) on Udemy
 
 My solutions & other work for the projects included in the [Python Pro Bootcamp 2023 by Dr. Angela Yu](https://www.udemy.com/course/100-days-of-code/).
+
+### Day 1 - Beginner - Working with variables in Python to manage data
+* [Solution: Cover Band name generator](/Day-001_band-name-generator/.)
