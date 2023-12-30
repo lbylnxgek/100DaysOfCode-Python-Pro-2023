f995945b086b97fd668a16843cddbd6c96ec15f1
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2023-12-30 13:55:27 -0700

    Add comments and minor code cleanup

--- Day-008/project/caesar_cipher.py
+++ Day-008/project/caesar_cipher.py
@@ -6,16 +6,23 @@ logo = art.logo
 def caesar(input_text, shift_number, cipher_direction):
     output_text = ""
 
-    for letter in input_text:
-        if letter not in alphabet:
-            output_text += letter
+    # Process each char in the input_text
+    for char in input_text:
+        # If char is not a letter, just add it to the output_text
+        if char not in alphabet:
+            output_text += char
+        # char is a letter
         else:
-            position = alphabet.index(letter)
+            # Determine index in alphabet list
+            position = alphabet.index(char)
+            # encode chosen, use position + shift_number for cipher letter
             if cipher_direction == "encode":
-                output_letter = alphabet[position + shift_number]
+                output_char = alphabet[position + shift_number]
+            # decode chosen, use position - shift_number for plain letter
             else:
-                output_letter = alphabet[position - shift_number]
-            output_text += output_letter
+                output_char = alphabet[position - shift_number]
+            # Add char to output_text
+            output_text += output_char
 
     print(f"The {direction}d text is: {output_text}\n")
 
@@ -35,3 +42,5 @@ while go_again == "y":
 
     caesar(input_text=text, shift_number=shift, cipher_direction=direction)
     go_again = input("Type 'y' if you want to go again: ").lower()
+
+print("\nGoodbye!")
