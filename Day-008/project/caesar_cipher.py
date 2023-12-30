95cbef8f2ebadcd67445d01564295576cecaab6e
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2023-12-30 13:23:23 -0700

    Update Day-008 solution, complete to UX improvements

--- Day-008/project/caesar_cipher.py
+++ Day-008/project/caesar_cipher.py
@@ -1,21 +1,37 @@
 import cipher_letters
+import art
 alphabet = cipher_letters.alphabet
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+logo = art.logo
 
 def caesar(input_text, shift_number, cipher_direction):
     output_text = ""
 
     for letter in input_text:
-        position = alphabet.index(letter)
-        if cipher_direction == "encode":
-            output_letter = alphabet[position + shift_number]
+        if letter not in alphabet:
+            output_text += letter
         else:
-            output_letter = alphabet[position - shift_number]
-        output_text += output_letter
+            position = alphabet.index(letter)
+            if cipher_direction == "encode":
+                output_letter = alphabet[position + shift_number]
+            else:
+                output_letter = alphabet[position - shift_number]
+            output_text += output_letter
 
-    print(f"The {direction}d text is: {output_text}")
+    print(f"The {direction}d text is: {output_text}\n")
 
-caesar(input_text=text, shift_number=shift, cipher_direction=direction)
+print(logo)
+
+go_again = "y"
+while go_again == "y":
+    direction = "none"
+    while direction != "encode" and direction != "decode":
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
+
+    text = input("Type your message: ").lower()
+
+    shift = 0
+    while shift < 1 or shift > 25:
+        shift = int(input("Type the shift number (1-25): "))
+
+    caesar(input_text=text, shift_number=shift, cipher_direction=direction)
+    go_again = input("Type 'y' if you want to go again: ").lower()
