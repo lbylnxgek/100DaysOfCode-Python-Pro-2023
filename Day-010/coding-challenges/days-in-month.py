fef8b80e2f7386d12346e89066fa7f9492efef7a
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2023-12-31 19:54:51 -0700

    Add docstrings and cleanup formatting

--- Day-010/coding-challenges/days-in-month.py
+++ Day-010/coding-challenges/days-in-month.py
@@ -1,29 +1,49 @@
 # Leap year function from earlier, modified to return True or False
 def is_leap(year):
-  if year % 4 == 0:
-    if year % 100 == 0:
-      if year % 400 == 0:
-        return True
-      else:
-        return False
+    """Return True if year is a leap year or False if not"""
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
     else:
-      return True
-  else:
-    return False
+        return False
+
 
 def days_in_month(year, month):
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    """Return the number of days for a year and month combination
+    taking into account leap years, in which February has 29 days."""
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-  # Check for leap year and month 2, which has 29 days
-  if is_leap(year) and month == 2:
-    return 29
+    # Check for leap year and month 2, which has 29 days
+    if is_leap(year) and month == 2:
+        return 29
 
-  # Determine number of days in month using month_days
-  return month_days[month -1]
+    # Determine number of days in month using month_days
+    return month_days[month - 1]
+
 
 def month_number_to_name(month_num):
-  month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-  return month_name[month_num]
+    """Take a month number and return the month name."""
+    month_name = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+    return month_name[month_num]
+
 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month (1-12): "))
