9f3077b77949a88843be2e0dbf7e3ea80e206ae5
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-01-02 20:06:46 -0700

    Add rough outline as TODO items

---
+++ Day-012/project/guess-the-number.py
@@ -0,0 +1,14 @@
+# TODO: Create ASCII artwork & add to art.py file
+
+# TODO: Print logo & welcome message
+# NOTE: The game only needs to run once
+
+# TODO: Create function to select a random number between 1-100
+# NOTE: In testing mode the selected number should be printed
+
+# TODO: Ask the user if they want to play on the easy (10 guesses)
+# or hard (5 guesses) level
+
+# TODO: Prompt user for guess.  Check guess and tell user too high/low
+# Subtract from remaining guesses as necessary or tell them if they guessed correctly
+# If there are no guesses remaining, then the game is over
