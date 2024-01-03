cca78f397f1494599207004fbd3840efd2133f33
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-01-03 13:13:25 -0700

    Add Day-012 ASCII art logo and completed code
    Code is functional, may need some improvement based on
    the instructor's walk-thru

--- Day-012/project/guess-the-number.py
+++ Day-012/project/guess-the-number.py
@@ -1,14 +1,48 @@
-# TODO: Create ASCII artwork & add to art.py file
+import random
+from art import logo
 
-# TODO: Print logo & welcome message
-# NOTE: The game only needs to run once
 
-# TODO: Create function to select a random number between 1-100
-# NOTE: In testing mode the selected number should be printed
+def check_guess(player_guess, random_number):
+    """Check player guess against number, return low, high or match"""
+    if player_guess < random_number:
+        return "low"
+    elif player_guess > random_number:
+        return "high"
+    else:
+        return "match"
 
-# TODO: Ask the user if they want to play on the easy (10 guesses)
-# or hard (5 guesses) level
 
-# TODO: Prompt user for guess.  Check guess and tell user too high/low
-# Subtract from remaining guesses as necessary or tell them if they guessed correctly
-# If there are no guesses remaining, then the game is over
+# Print logo and welcome information
+print(logo)
+print("Welcome to Guess the Number!")
+print("I'm thinking of a number between 1 and 100.")
+
+# Pick a random number
+number = random.randint(1, 100)
+# DEBUG: Print random number
+print(f"DEBUG: The number is {number}.")
+
+# Prompt user for a difficulty level, set guesses_remaining
+difficulty_level = input("Choose a difficulty level.  Type 'easy' or 'hard': ").lower()
+if difficulty_level == "easy":
+    guesses_remaining = 10
+else:
+    guesses_remaining = 5
+
+game_over = False
+while not game_over:
+    print(f"You have {guesses_remaining} guesses remaining.")
+    guess = int(input("What is your guess? "))
+    result = check_guess(player_guess=guess, random_number=number)
+    if result == "low":
+        print("Too low.")
+        guesses_remaining -= 1
+    elif result == "high":
+        print("Too high.")
+        guesses_remaining -= 1
+    elif result == "match":
+        print(f"Winner, winner, chicken dinner!  The number was {number}.")
+        game_over = True
+    if guesses_remaining == 0:
+        print(f"\nYou are out of guesses, the number was {number}.")
+        game_over = True
