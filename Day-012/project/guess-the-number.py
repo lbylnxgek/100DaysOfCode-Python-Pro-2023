66c34eeb19d434739c79570bb7ec996b22572023
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-01-03 18:36:41 -0700

    Move setting the difficulty to a function
    Incorporate global variables to make changing the number
    of guesses for each level easier

--- Day-012/project/guess-the-number.py
+++ Day-012/project/guess-the-number.py
@@ -1,6 +1,11 @@
 import random
 from art import logo
 
+# Set global constants here, as it makes changing the difficulty easy since
+# no code modification is necessary.
+EASY_LEVEL_GUESSES = 10
+HARD_LEVEL_GUESSES = 5
+
 
 def check_guess(player_guess, random_number):
     """Check player guess against number, return low, high or match"""
@@ -10,6 +15,14 @@ def check_guess(player_guess, random_number):
         return "high"
     else:
         return "match"
+
+
+def set_difficulty_level():
+    level = input("Choose a difficulty level.  Type 'easy' or 'hard': ").lower()
+    if level == "easy":
+        return EASY_LEVEL_GUESSES
+    else:
+        return HARD_LEVEL_GUESSES
 
 
 # Print logo and welcome information
@@ -22,12 +35,8 @@ number = random.randint(1, 100)
 # DEBUG: Print random number
 print(f"DEBUG: The number is {number}.")
 
-# Prompt user for a difficulty level, set guesses_remaining
-difficulty_level = input("Choose a difficulty level.  Type 'easy' or 'hard': ").lower()
-if difficulty_level == "easy":
-    guesses_remaining = 10
-else:
-    guesses_remaining = 5
+# Set number of guesses
+guesses_remaining = set_difficulty_level()
 
 game_over = False
 while not game_over:
