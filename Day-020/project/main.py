e965a92628039a8e7d68ce401cc857047fed1f71
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-01-21 16:56:55 -0700

    Add Day 20 Snake Game task 1: Create snake body

---
+++ Day-020/project/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+all_segments = []
+
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    all_segments.append(new_segment)
+
+
+screen.exitonclick()
