e862f37222d476c251beb7147acd38236cf3b149
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-01-24 19:36:36 -0700

    Update Day 21 Snake Game task 3: Detect collisions with food

--- Day-020+021/project/main.py
+++ Day-020+021/project/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 # Difficulty level: Smaller number = faster snake
@@ -12,6 +13,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -22,8 +25,9 @@ game_on = True
 while game_on:
     screen.update()
     time.sleep(SLEEP_TIME)
-
     snake.move()
-
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
