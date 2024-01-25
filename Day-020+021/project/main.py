a6c0b3610455dc3d517467ac0718cc9a717bf7ac
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-01-24 20:27:26 -0700

    Update Day 21 Snake Game task 4: Create scoreboard

--- Day-020+021/project/main.py
+++ Day-020+021/project/main.py
@@ -1,10 +1,11 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 # Difficulty level: Smaller number = faster snake
-SLEEP_TIME = 1.0
+SLEEP_TIME = 0.2
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -14,6 +15,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -29,5 +31,7 @@ while game_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
+        scoreboard.update_scoreboard()
 
 screen.exitonclick()
---
+++ Day-020+021/project/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 10, "bold")
+
+
+class Scoreboard(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.score = 0
+        self.pencolor("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 280)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(
+            f"Score: {self.score}",
+            move=False,
+            align=ALIGNMENT,
+            font=FONT,
+        )
+
+    def increase_score(self):
+        self.score += 1
