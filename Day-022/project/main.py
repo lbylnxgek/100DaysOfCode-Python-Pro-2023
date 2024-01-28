6c17dafd3a6fabb7e8ed69b81d411d1e600728e3
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-01-27 20:02:08 -0700

    Add Day 22 task 2: Create and move paddle

--- Day-022/project/main.py
+++ Day-022/project/main.py
@@ -1,5 +1,4 @@
-from turtle import Turtle
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -25,6 +24,34 @@ for _ in range(0, 29):
 
 center_line.hideturtle()
 screen.update()
+
+
+def paddle_up():
+    new_y = r_paddle.ycor() + 20
+    r_paddle.goto(r_paddle.xcor(), new_y)
+
+
+def paddle_down():
+    new_y = r_paddle.ycor() - 20
+    r_paddle.goto(r_paddle.xcor(), new_y)
+
+
+# Create right paddle
+r_paddle = Turtle()
+r_paddle.shape("square")
+r_paddle.color("white")
+r_paddle.shapesize(stretch_len=1, stretch_wid=5)
+r_paddle.penup()
+r_paddle.goto(350, 0)
+screen.update()
+
+screen.listen()
+screen.onkey(paddle_up, "Up")
+screen.onkey(paddle_down, "Down")
+
+game_on = True
+while game_on:
+    screen.update()
 
 
 screen.exitonclick()
