25dd2d1be4152432fe40b513063b4cb0f1154f25
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-02-04 22:01:38 -0700

    Add Day 23 task 5: Scoreboard
    Tasks:
      - Add scoreboard & display level
      - Increase level & redisplay when player crosses finishline
      - Increase speed of cars

--- Day-023/main.py
+++ Day-023/main.py
@@ -9,11 +9,11 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-
 screen.listen()
 screen.onkeypress(player.move, "Up")
 
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 game_on = True
 while game_on:
@@ -25,11 +25,13 @@ while game_on:
     # Detect collision with car
     for car in car_manager.all_cars:
         if player.distance(car) < 20:
+            scoreboard.game_over()
             game_on = False
 
     # Detect if player has crossed the finish line
     if player.at_finish_line():
         player.go_to_start()
+        scoreboard.increase_level()
         car_manager.increase_speed()
 
 screen.exitonclick()
