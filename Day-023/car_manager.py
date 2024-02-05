4982e3bab168f837e3880e8c9f64eab805344518
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-02-04 20:07:17 -0700

    Update Day 23 task 2: Create & move cars
    Update code so there's a 1 in 6 chance of a new car being
    created instead of every 6 loops of the main while loop

--- Day-023/car_manager.py
+++ Day-023/car_manager.py
@@ -12,15 +12,17 @@ class CarManager:
         self.all_cars = []
 
     def create_car(self):
-        new_car = Turtle()
-        new_car.shape("square")
-        new_car.shapesize(stretch_len=2, stretch_wid=1)
-        new_car.color(random.choice(COLORS))
-        new_car.penup()
-        new_car.setheading(180)
-        new_car.y_start_pos = random.randint(-250, 250)
-        new_car.goto(300, new_car.y_start_pos)
-        self.all_cars.append(new_car)
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle()
+            new_car.shape("square")
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.color(random.choice(COLORS))
+            new_car.penup()
+            new_car.setheading(180)
+            new_car.y_start_pos = random.randint(-250, 250)
+            new_car.goto(300, new_car.y_start_pos)
+            self.all_cars.append(new_car)
 
     def move_cars(self):
         for car in self.all_cars:
