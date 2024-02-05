6487cd2e3d1241aa7ffc30398542edac3c59d592
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-02-04 20:03:39 -0700

    Add Day 23 task 2: Create & move cars

--- Day-023/main.py
+++ Day-023/main.py
@@ -13,10 +13,20 @@ player = Player()
 screen.listen()
 screen.onkeypress(player.move, "Up")
 
+car_manager = CarManager()
+
 game_on = True
+loop_count = 0
 while game_on:
     time.sleep(0.1)
+    # print(f"DEBUG: loop_count:{loop_count}")
+    # Create new car every 6th loop
+    if loop_count % 6 == 0:
+        car_manager.create_car()
+    car_manager.move_cars()
     screen.update()
+
+    loop_count += 1
 
 
 screen.exitonclick()
