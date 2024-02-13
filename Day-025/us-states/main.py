5e0c7634a3ba957fd9604f0ad142a39268981f94
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-02-12 19:14:58 -0700

    Update Day 25 code: minor code cleanup & commenting

--- Day-025/us-states/main.py
+++ Day-025/us-states/main.py
@@ -7,6 +7,7 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+# Create turtle to write state names
 state_turtle = turtle.Turtle()
 state_turtle.hideturtle()
 state_turtle.penup()
@@ -26,7 +27,22 @@ while len(correct_guesses) < 50:
         title=f"{len(correct_guesses)}/50 states correct",
         prompt="What's another state's name?",
     ).title()
-    print(answer_state)
+
+    # Check for exit condition
+    if answer_state == "Exit":
+        # Create list of states to learn
+        states_to_learn = []
+        for state in states_list:
+            if state not in correct_guesses:
+                states_to_learn.append(state)
+
+        # Create dictionary
+        data_dict = {"State": states_to_learn}
+
+        # Create dataframe and write to CSV
+        df = pandas.DataFrame(data_dict)
+        df.to_csv("states_to_learn.csv")
+        break
 
     # Check for correct guess
     if answer_state in states_list and answer_state not in correct_guesses:
@@ -34,6 +50,3 @@ while len(correct_guesses) < 50:
         state_turtle.goto(int(state_data.x), int(state_data.y))
         state_turtle.write(state_data.state.item())
         correct_guesses.append(state_data.state.item())
-        print(correct_guesses)
-
-screen.exitonclick()
