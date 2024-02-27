5f36928923c14f00eed15077365069b589c210d1
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-02-26 19:38:11 -0700

    Update Day 29 code: Password generator GUI
      - Complete UI
        - Add labels
        - Add entries
        - Add buttons
    
    I used different width values to get the columns to line up
    reasonably well.  It would be interesting to see how it looks
    on a different OS.

--- Day-029/main.py
+++ Day-029/main.py
@@ -12,7 +12,30 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=50)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+account_label = Label(text="Email/Username:")
+account_label.grid(row=2, column=0)
+
+account_entry = Entry(width=50)
+account_entry.grid(row=2, column=1, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+password_entry = Entry(width=32)
+password_entry.grid(row=3, column=1)
+
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+
+add_record_button = Button(text="Add", width=42)
+add_record_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
