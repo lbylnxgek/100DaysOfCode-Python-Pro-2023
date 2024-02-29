ea30a4bce098ba6c6edf7c88cff5fe192acd5d54
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-02-28 20:53:10 -0700

    Update Day 30 starting code: NATO phonetic alphabet redux
      - Convert to function
      - Add try/except to catch non-alphabetical input

--- Day-030/nato-alphabet/main.py
+++ Day-030/nato-alphabet/main.py
@@ -6,8 +6,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(nato_dict)
 
-word = input("Enter a word: ").upper()
-# print(word)
 
-word_phonetic = [nato_dict[letter] for letter in word]
-print(word_phonetic)
+def convert_to_nato_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        word_phonetic = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet allowed.")
+        convert_to_nato_phonetic()
+    else:
+        print(word_phonetic)
+
+
+convert_to_nato_phonetic()
