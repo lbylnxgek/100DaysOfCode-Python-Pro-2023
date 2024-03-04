7574cd0b9a73ed020bc3f699e217d4bfea71d4ac
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-03-03 20:33:32 -0700

    Update Day 31: Flash card app
    Flip card:
      - Change the card image
      - Change the card text color
      - Change card_title to "English"
      - Change card_word to the corresponding English word
      - Add timer to automatically flip card after 3 seconds
      - Reset card image & text color when creating new card

--- Day-031/main.py
+++ Day-031/main.py
@@ -3,16 +3,27 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
 data = pandas.read_csv("./data/french_words.csv")
 words_to_learn = data.to_dict(orient="records")
+random_word = {}
 
 
 # ---------------------------  CREATE CARD ----------------------------- #
 def create_card():
+    global random_word, flip_timer
+    window.after_cancel(flip_timer)
     random_word = random.choice(words_to_learn)
-    canvas.itemconfig(card_title, text=f"French")
-    canvas.itemconfig(card_word, text=f"{random_word["French"]}")
+    canvas.itemconfig(card_image, image=card_front_img)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=random_word["French"], fill="black")
+    flip_timer = window.after(3000, func=flip_card)
+
+
+# ---------------------------  FLIP CARD ------------------------------ #
+def flip_card():
+    canvas.itemconfig(card_image, image=card_back_img)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=random_word["English"], fill="white")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -20,9 +31,12 @@ window = Tk()
 window.title("Flash Me! - French to English")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="./images/card_back.png")
+card_image = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
