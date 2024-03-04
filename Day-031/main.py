3ca9fac5251973db7b5d7f09a8701f54e6985609
Eric Sisler <12535834+lbylnxgek@users.noreply.github.com>
2024-03-03 21:50:32 -0700

    Update Day 31: Flash card app
    Save progress:
      - When user checks that the know the word, remove it from the list
      - When user checks they don't know the word, add it to
        "words_to_learn.csv"
      - Add try/except block to check for "words_to_learn.csv" file

--- Day-031/main.py
+++ Day-031/main.py
@@ -3,27 +3,39 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-data = pandas.read_csv("./data/french_words.csv")
+
+try:
+    data = pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("./data/french_words.csv")
+
 words_to_learn = data.to_dict(orient="records")
-random_word = {}
+current_word = {}
 
 
-# ---------------------------  CREATE CARD ----------------------------- #
 def create_card():
-    global random_word, flip_timer
+    global current_word, flip_timer
     window.after_cancel(flip_timer)
-    random_word = random.choice(words_to_learn)
+    current_word = random.choice(words_to_learn)
     canvas.itemconfig(card_image, image=card_front_img)
     canvas.itemconfig(card_title, text="French", fill="black")
-    canvas.itemconfig(card_word, text=random_word["French"], fill="black")
+    canvas.itemconfig(card_word, text=current_word["French"], fill="black")
     flip_timer = window.after(3000, func=flip_card)
 
 
-# ---------------------------  FLIP CARD ------------------------------ #
 def flip_card():
     canvas.itemconfig(card_image, image=card_back_img)
     canvas.itemconfig(card_title, text="English", fill="white")
-    canvas.itemconfig(card_word, text=random_word["English"], fill="white")
+    canvas.itemconfig(card_word, text=current_word["English"], fill="white")
+
+
+def word_is_known():
+    words_to_learn.remove(current_word)
+    print(len(words_to_learn))
+    data = pandas.DataFrame(words_to_learn)
+    data.to_csv("./data/words_to_learn.csv", index=False)
+
+    create_card()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -47,7 +59,7 @@ unknown_button = Button(image=unknown_image, highlightthickness=0, command=creat
 unknown_button.grid(row=1, column=0)
 
 known_image = PhotoImage(file="./images/right.png")
-known_button = Button(image=known_image, highlightthickness=0, command=create_card)
+known_button = Button(image=known_image, highlightthickness=0, command=word_is_known)
 known_button.grid(row=1, column=1)
 
 create_card()
